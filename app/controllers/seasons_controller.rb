9bc29f6d2dc0648897994f9db03b0f1213af5ef5
Steven Bertolucci <srbertol@mtu.edu>
2018-05-14 11:20:19 -0700

    Change seasons response to conform to spec

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,20 +1,21 @@
 class SeasonsController < ApplicationController
-  before_action :set_season, only: [:show, :update, :destroy]
+
+  before_action :set_season, only: %i[show update destroy]
 
   # GET /seasons
   def index
     @seasons = Season.all
-
-    render json: @seasons
+    render json: { seasons: @seasons }
   end
 
   # GET /seasons/1
   def show
-    render json: @season
+    render json: { seasons: @season }
   end
 
   # POST /seasons
   def create
+
     @season = Season.new(season_params)
 
     if @season.save
@@ -22,15 +23,18 @@ class SeasonsController < ApplicationController
     else
       render json: @season.errors, status: :unprocessable_entity
     end
+
   end
 
   # PATCH/PUT /seasons/1
   def update
+
     if @season.update(season_params)
       render json: @season
     else
       render json: @season.errors, status: :unprocessable_entity
     end
+
   end
 
   # DELETE /seasons/1
@@ -39,13 +43,16 @@ class SeasonsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_season
-      @season = Season.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def season_params
-      params.fetch(:season, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_season
+    @season = Season.find(params[:id])
+    #@season.links = { episodes: }
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def season_params
+    params.fetch(:season, {})
+  end
+
 end
